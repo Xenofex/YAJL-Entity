8a999a646d8e99fbd2b6204bac96655421c07660
Eli Wang <eli@ekohe.com>
2013-10-17 14:52:19 +0800

    Initial Commit

---
+++ YAJL-Entity/YAJL_Entity.h
@@ -0,0 +1,13 @@
+//
+//  YAJL_Entity.h
+//  YAJL-Entity
+//
+//  Created by Eli Wang on 13-10-17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YAJL_Entity : NSObject
+
+@end
---
+++ YAJL-Entity/YAJL_Entity.m
@@ -0,0 +1,13 @@
+//
+//  YAJL_Entity.m
+//  YAJL-Entity
+//
+//  Created by Eli Wang on 13-10-17.
+//
+//
+
+#import "YAJL_Entity.h"
+
+@implementation YAJL_Entity
+
+@end
